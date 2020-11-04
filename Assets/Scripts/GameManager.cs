24dfd586e74c600d1bbcd1d1a25fbe63d401d6df
sylvainBenoist <benoistsylvain33@gmail.com>
2020-11-04 02:04:42 +0100

    Push monde Econstruction + intégration Sylvain
    Intégration moteur de jeu :
    - Création route
    - Création de maison
    - Création de store
    - Création banque
    - Ajout asset éolienne + création éolienne
    
    Changement sur le GUI :
    - Rajout bouton Spécial, Bank, Eolienne, Destroy
    
    Destroy non fonctionnel

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using SVS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CameraMovement cameraMovement;
+    public RoadManager roadManager;
+    public InputManager inputManager;
+
+    public UIController uiController;
+
+    public StructureManager structureManager;
+    public PlacementManager placementManager;
+
+    private void Start()
+    {
+        uiController.OnRoadPlacement += RoadPlacementHandler;
+        uiController.OnHousePlacement += HousePlacementHandler;
+        uiController.OnSpecialPlacement += SpecialPlacementHandler;
+        uiController.OnBankPlacement += BankPlacementHandler;
+        uiController.OnEoliennePlacement += EolienneHandler;
+        uiController.OnDestroyPlacement += DestroyHandler;
+        
+    }
+    private void EolienneHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceEolienne;
+    }
+    private void DestroyHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.DestroyAssets;
+    }
+
+    private void SpecialPlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceSpecial;
+    }
+
+    private void HousePlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceHouse;
+    }
+    private void BankPlacementHandler()
+    {
+        ClearInputActions();
+        inputManager.OnMouseClick += structureManager.PlaceBank;
+    }
+
+    private void RoadPlacementHandler()
+    {
+        ClearInputActions();
+
+        inputManager.OnMouseClick += roadManager.PlaceRoad;
+        inputManager.OnMouseHold += roadManager.PlaceRoad;
+        inputManager.OnMouseUp += roadManager.FinishPlacingRoad;
+    }
+
+    private void ClearInputActions()
+    {
+        inputManager.OnMouseClick = null;
+        inputManager.OnMouseHold = null;
+        inputManager.OnMouseUp = null;
+    }
+
+    private void Update()
+    {
+        cameraMovement.MoveCamera(new Vector3(inputManager.CameraMovementVector.x,0, inputManager.CameraMovementVector.y));
+    }
+}
